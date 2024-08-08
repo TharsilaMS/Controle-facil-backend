9ef58e1f327ef3cb2a4535935cdd97f61e42e3e5
TharsilaMS <tharsilams@gmail.com>
2024-08-07 21:29:25 -0300

    Iniciando o projeto

---
+++ src/main/java/com/controlefacil/controlefacil/ControlefacilApplication.java
@@ -0,0 +1,13 @@
+package com.controlefacil.controlefacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlefacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlefacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/controlefacil/controlefacil/ControlefacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controlefacil.controlefacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlefacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
